2f4f64d58817a3d8660400df65befa01eadaaabc
KenyanJesus <s165249@student.dtu.dk>
2019-08-27 12:10:32 +0200

    make it better

---
+++ Main.js
@@ -0,0 +1,62 @@
+var Twit = require('twit')
+
+var fs = require('fs'),
+    path = require('path'),
+    Twit = require('twit'),
+    config = require(path.join(__dirname, 'config.js'));
+    //images = require(path.join(__dirname, 'images.js'));
+
+var T = new Twit(config);
+
+fs.readdir(__dirname + '/images', function(err, files) {
+    if (err){
+        console.log(err);
+    }
+    else{
+        var images = [];
+        files.forEach(function(f) {
+            images.push(f);
+        });
+
+        upload_random_image(images);
+        setInterval(function(){upload_random_image(images);}, 21600000);
+    }
+});
+
+
+function random_from_array(images){
+    return images[Math.floor(Math.random() * images.length)];
+}
+
+function upload_random_image(images){
+    console.log('Opening an image...');
+    var image_path = path.join(__dirname, '/images/' + random_from_array(images)),
+        b64content = fs.readFileSync(image_path, { encoding: 'base64' });
+
+    console.log('Uploading an image...');
+
+    T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+        if (err){
+            console.log('ERROR:');
+            console.log(err);
+        }
+        else{
+            console.log('Image uploaded!');
+            console.log('Now tweeting it...');
+
+            T.post('statuses/update', {
+                    media_ids: new Array(data.media_id_string)
+                },
+                function(err, data, response) {
+                    if (err){
+                        console.log('ERROR:');
+                        console.log(err);
+                    }
+                    else{
+                        console.log('Posted an image!');
+                    }
+                }
+            );
+        }
+    });
+}
