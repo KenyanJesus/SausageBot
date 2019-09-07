84ef6e7872543c4bab050ce8241ee6a469b2a041
KenyanJesus <s165249@student.dtu.dk>
2019-09-07 23:59:03 +0200

    even more dogs

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {
-    consumer_key: 'no1894QdDSix1iaaVZid1bFdM',
-    consumer_secret: 'vocKPqshQiwRJ6x0psPrwM2oMo16gRzWARN8jqAMxoeGgTj1uD',
-    access_token: '1165988154777788416-rdQvlfZrDpoxagtzlmn3XhARUJ0nge',
-    access_token_secret: 'X3inueUlPkJDjBHiqJ431JLN2SVD0CbuPcKdlYsLExFcL'
+    consumer_key: 'no',
+    consumer_secret: 'no',
+    access_token: 'no',
+    access_token_secret: 'no'
 }
 
 module.exports = config;
